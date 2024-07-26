585af2c43018510f298f231157a149dd04193f71
yarenunal <yareunal16@gmail.com>
2024-07-26 15:15:00 +0300

    Initial Commit

---
+++ TodosTutorial/TodosTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosTutorialApp.swift
+//  TodosTutorial
+//
+//  Created by Yaren on 26.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodosTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
